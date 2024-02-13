416e58f74f8c4a4b564e1f4a6b471d204be0bed9
lidlacgar <lidlacgar@INF1-PC15>
2024-02-13 10:33:18 +0100

    he afgit la classe motor()

---
+++ LaFormula1_AL/src/main/java/ies/montduver/laformula1/motor.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.montduver.laformula1;
+
+/**
+ *
+ * @author lidlacgar
+ */
+public class motor {
+    String tipo;
+    Double cilindrada;
+    int potencia; 
+
+    public motor() {
+    }
+
+    public motor(String tipo, Double cilindrada, int potencia) {
+        this.tipo = tipo;
+        this.cilindrada = cilindrada;
+        this.potencia = potencia;
+    }
+
+    @Override
+    public String toString() {
+        return "motor{" + '}';
+    }
+    
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public Double getCilindrada() {
+        return cilindrada;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setCilindrada(Double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    
+    
+    
+            
+    
+}
