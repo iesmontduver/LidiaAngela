09db5daa4ab7c701e4beafd2419d8fdbb8a48ffd
lidlacgar <lidlacgar@INF1-PC15>
2024-02-14 13:14:52 +0100

    he afgit la classe escuderia()

---
+++ LaFormula1_AL/src/main/java/ies/montduver/laformula1/Escuderia.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.montduver.laformula1;
+
+/**
+ *
+ * @author lidlacgar
+ */
+public class Escuderia {
+    String nom;
+    double pressupost; 
+    int punts; 
+
+    public Escuderia() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getPressupost() {
+        return pressupost;
+    }
+
+    public int getPunts() {
+        return punts;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPressupost(double pressupost) {
+        this.pressupost = pressupost;
+    }
+
+    public void setPunts(int punts) {
+        this.punts = punts;
+    }
+    
+    
+    
+}
